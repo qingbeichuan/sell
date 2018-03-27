b3880fc0064c415ccfd313bb328f0ec9fc754dcf
qingbeichuan <qingbeichuan@163.com>
2018-03-27 22:22:39 +0800

    页面骨架开发

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <!--头部组件-->
+    <sellHeader></sellHeader>
+    <!--选项卡-->
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商品</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import sellHeader from '@/components/header/header'
+  export default {
+    name: 'App',
+    components:{
+      sellHeader
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './common/stylus/mixin'
+  #app
+    .tab
+      display:flex
+      height:40px
+      line-height:40px
+      /*border-bottom:1px solid #ddd*/
+      border-1px(rgba(7, 17, 27, 0.1))
+      .tab-item
+        flex:1
+        text-align:center
+        a
+          display:block
+          font-size:14px
+          color: rgb(77, 85, 93)
+          &.router-link-active
+            color: rgb(240, 20, 20)
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="header">
+
+  </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped lang="stylus">
+
+</style>
